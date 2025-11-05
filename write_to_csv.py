cd090542df7463833bda14af799a522e933d544b
mariaroslyakova <165595568+mariaroslyakova@users.noreply.github.com>
2025-11-05 19:20:54 +0300

    Update write_to_csv.py

--- write_to_csv.py
+++ write_to_csv.py
@@ -3,7 +3,9 @@ import csv
 
 def write_rows_to_csv(filename, rows):
     with open(filename, mode='w',newline='', encoding='utf-8') as file:
+        # Создаем объект для записи данных в csv
         writer = csv.writer(file)
+        # Запись первой строки с названием колонн
         writer.writerow(['№','Цифровой код','Буквенный код','Единиц','Валюта','Курс'])
         dict = {}
         for row in rows:
